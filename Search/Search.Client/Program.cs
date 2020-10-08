45715c361edf7cc16412fc42294aeafcb5aa84ff
Fabian K <fabbemannen@gmail.com>
2020-10-08 15:07:35 +0200

    Added search engine for recipes (elasticsearch)

--- Search/Search.Client/Program.cs
+++ Search/Search.Client/Program.cs
@@ -3,6 +3,7 @@ using Elasticsearch.Net;
 using Search.Client.Options;
 using Search.Client.Services;
 using Search.Common.Models;
+using Search.Common.Models.Dto;
 using Search.Common.Services;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,56 @@ namespace Search.Client
             // 3. Räkna alla recept som är upplagda av Per Morberg.
             // 4. Hitta 30 recept som tillhör kategorin Bönor.
             // 5. Räkna alla recept som har en tillagningstid på under 10 minuter.
+
+            var searchResponse1and2 = client
+                .Search(s => s
+                    .Sort(ss => ss
+                        .Descending(p => p.Rating)
+                    )
+                    .Size(20)
+                    .Query(q => q
+                        .Match(m => m
+                            .Query(options.Query)
+                            )
+                        )
+                    );
+
+            var searchResponse3 = client
+                .Count(s => s
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.Author)
+                            .Query("Per Morberg")
+                            )
+                        )
+                    );
+
+
+            var searchResponse4 = client
+                .Search(s => s
+                    .Size(30)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.Categories)
+                            .Query("Bönor")
+                            )
+                        )
+                    );
+
+            var searchResponse5 = client
+                .Count(s => s
+                    .Query(q => q
+                        .Bool(b => b
+                            .Filter(bf => bf
+                                .Range(r => r
+                                    .Field(f => f.TimeToCook)
+                                    .LessThanOrEquals(new TimeSpan(0, 10, 0).Ticks)
+                                )
+                            )
+                        )
+
+                    )
+                );
 
             return 0;
         }
